61ddb553714ba9af786c328ee936bcf60e0ffaa7
humor <smilelife_xilei@163.com>
2024-06-03 23:38:36 +0800

    0603

---
+++ 110.平衡二叉树.java
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode.cn/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (57.96%)
+ * Likes:    1472
+ * Dislikes: 0
+ * Total Accepted:    568K
+ * Total Submissions: 979.4K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int left = isBalancedDeep(root.left);
+        int right = isBalancedDeep(root.right);
+        return left != -1 && right != -1 && Math.abs(left - right) <= 1;
+    }
+
+    public int isBalancedDeep(TreeNode node) {
+        if (node == null) return 0;
+        int left = isBalancedDeep(node.left);
+        int right = isBalancedDeep(node.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        } else{
+            return Math.max(left, right) + 1;
+        }
+    }
+}
+// @lc code=end
+
---
+++ 111.二叉树的最小深度.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=java
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (53.18%)
+ * Likes:    1146
+ * Dislikes: 0
+ * Total Accepted:    636.8K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ * 
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 
+ * 说明：叶子节点是指没有子节点的节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数的范围在 [0, 10^5] 内
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
+// @lc code=end
+
